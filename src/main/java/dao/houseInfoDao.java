96aea633a549b082b68357985083ad3f91d39c08
醉家男煮饺 <402050821@qq.com>
2019-03-13 22:57:18 +0800

    增加insert语句

--- src/main/java/dao/houseInfoDao.java
+++ src/main/java/dao/houseInfoDao.java
@@ -4,7 +4,9 @@ import entity.houseInfo;
 import org.apache.ibatis.annotations.Insert;
 
 public interface houseInfoDao {
-
+//INSERT INTO `crawlhouse`.`rentHouse` (`id`, `title`, `monthlyRent`, `payType`, `rentType`, `roomType`,
+// `area`, `floor`, `addr`, `decoration`, `releaseDate`, `fristImg`)
+// VALUES ('1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1');
     @Insert("insert into houseInfo (`houseType`,`direction`,`area`,`monthlyRent`,`sellType`,`releaseDate`,`floor`,`addr`) " +
             "values (#{houseType},#{direction},#{area},#{monthlyRent},#{sellType},#{releaseDate},#{floor},#{addr})")
     public int add(houseInfo houseInfo);
